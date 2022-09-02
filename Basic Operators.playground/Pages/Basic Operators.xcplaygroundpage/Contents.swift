5311e1c73aa9b609cc400bf9235c7d67415fd79e
Matthew Harding <matthewpharding@gmail.com>
2022-09-02 23:14:22 +0100

    Tidied up and finished the last iteration before completing the lesson version 1. The README file now contains the differences from the playground file that will make it more understandable due to the differences in the markdown language between the .playground and .md file

--- Basic Operators.playground/Pages/Basic Operators.xcplaygroundpage/Contents.swift
+++ Basic Operators.playground/Pages/Basic Operators.xcplaygroundpage/Contents.swift
@@ -2,7 +2,8 @@
 //: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/BasicOperators.html)
 //: # Basic Operators
 //:
-//: An operator is a special symbol or phrase that you use to check, change, or combine values. For example, the addition operator (`+`) adds two numbers, as in `let i = 1 + 2`, and the logical AND operator (`&&`) combines two Boolean values, as in `if enteredDoorCode` && passedRetinaScan`.
+//:
+//: An operator is a special symbol or phrase that you use to check, change, or combine values. For example, the addition operator (`+`) adds two numbers, as in `let i = 1 + 2`, and the logical AND operator (`&&`) combines two Boolean values, as in `if enteredDoorCode && passedRetinaScan`.
 //:
 //: Swift supports the operators you may already know from languages like C, and improves several capabilities to eliminate common coding errors. The assignment operator (`=`) doesn’t return a value, to prevent it from being mistakenly used when the equal to operator (`==`) is intended. Arithmetic operators (`+`, `-`, `*`, `/`, `%` and so forth) detect and disallow value overflow, to avoid unexpected results when working with numbers that become larger or smaller than the allowed value range of the type that stores them. You can opt in to value overflow behavior by using Swift’s overflow operators, as described in Overflow Operators.
 //:
@@ -37,7 +38,7 @@ let (x, y) = (1, 2)
 //if x = y {
 //    // This isn't valid, because x = y doesn't return a value.
 //}
-//: This feature prevents the assignment operator (`=`) from being used by accident when the equal to operator (`==`) is actually intended. By making if `x = y` invalid, Swift helps you to avoid these kinds of errors in your code.
+//: This feature prevents the assignment operator (`=`) from being used by accident when the equal to operator (`==`) is actually intended. By making `if x = y` invalid, Swift helps you to avoid these kinds of errors in your code.
 //:
 //: ## Arithmetic Operators
 //:
